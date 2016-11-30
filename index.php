5bb3c061efac8cb1c81621e42b54c6e8277eb120
HoYean <comforest@naver.com>
2016-11-30 16:10:25 +0900

    Conservation API

--- index.php
+++ index.php
@@ -6,6 +6,43 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<h1> HI </h1>
+<?php 
+
+    $workspace_id = 'cc934d4e-0130-403c-b6e9-df4b78e43c89';
+    $userName = "91184ae8-7f0f-4852-bc04-da75712108cb";
+    $userPwd = "UYmCRYQLd2EG";
+
+    $data = array("input" => array("text"=>"Ashe is the most popular champion?"));   
+    $data_string = json_encode($data);
+
+    // create curl resource 
+    $ch = curl_init(); 
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/$workspace_id/message?version=2016-09-20"); 
+
+    //return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_USERPWD, "$userName:$userPwd");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+    );
+       
+
+    // $output contains the output string 
+    $out = curl_exec($ch);
+    
+    echo $out.'<Br><br>';
+    $arr = json_decode($out);
+
+    curl_close($ch);      
+
+
+?>
 </body>
 </html>
