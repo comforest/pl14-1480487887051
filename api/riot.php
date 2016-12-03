5cad2be906fb8df341fe4741c4ab7fcde026d223
cyclone472 <cyclone472@naver.com>
2016-12-04 00:41:46 +0900

    add rot_champ()

--- api/riot.php
+++ api/riot.php
@@ -6,20 +6,32 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-<?php
-  $apiKey = "RGAPI-040244e7-c5c4-4f93-910b-07b367bf78ca";
-  $region = "kr";
+<?php 
+  function rot_champ() {
+	$url1 = "https://kr.api.pvp.net/api/lol/kr/v1.2/champion?api_key=RGAPI-040244e7-c5c4-4f93-910b-07b367bf78ca";
+	$region = "kr";
+	$ch1 = curl_init(); 
 
-  $url = "https://kr.api.pvp.net/api/lol/$region/v1.2/champion?api_key=$apiKey";
-
-  $ch = curl_init(); 
-
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch1, CURLOPT_URL, $url1);
    
-  $output = curl_exec($ch);
-  echo $output;
+	$output = curl_exec($ch1);
+	$decode = json_decode($output, true);
+    curl_close($ch1);
+	echo $arr;
+	
+	$result = array();
+	$arr2 = $decode['champions'];
+	foreach($arr2 as $key => $value) {
+		if($value["freeToPlay"] === true) {
+			$result[] = $value['id'];
+		}
+	}
+	print_r($result);
+  }
+  
+  rot_champ();
 ?>
 </body>
 </html>
